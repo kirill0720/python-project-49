241aa1ef6236e22ed0052b2571806a2a29f6ba58
kirill0720 <kirill.netreba@gmail.com>
2022-10-06 01:21:31 +0300

    index out of range error fix

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -6,7 +6,7 @@ RULE = 'What number is missing in the progression?'
 def generate_qa():
     p_len = random.randint(5, 10)  # length of progression
     p_start = random.randint(0, 90)
-    p_lst = [str(x) for x in range(p_start, p_start + p_len)]
+    p_lst = [str(x) for x in range(p_start, p_start + p_len + 1)]
     ind = random.randint(0, p_len)  # index of hidden element
     answer = p_lst[ind]
     p_lst[ind] = '..'
