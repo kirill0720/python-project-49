edd04b86159fd12a2ed293c5c92ad30305729dcb
kirill0720 <kirill.netreba@gmail.com>
2022-10-05 17:01:16 +0300

    step 3

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+import brain_games.cli
+
+
 def main():
-    print('poetry run python -m brain_games.scripts.brain_games')
-    print('Welcome to the Brain Games!')
+    brain_games.cli.welcome_user()
 
 
 if __name__ == '__main__':
