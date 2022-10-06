4d18bcfeac5ad57882104322445fb6a196d0a007
kirill0720 <kirill.netreba@gmail.com>
2022-10-06 13:59:26 +0300

    fix magic numbers

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -4,7 +4,9 @@ import prompt
 def play(rule, func):
     '''Engine for brain-like games.
     INPUTS: rule message and a function to generate questions and answers.'''
-
+    
+    correct_count_limit = 3  # number of correct answer to exit the game
+    
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
@@ -12,7 +14,7 @@ def play(rule, func):
     print(rule)
 
     correct_count = 0
-    while correct_count < 3:
+    while correct_count < correct_count_limit:
         question, ans = func()
         print(f'Question: {question}')
         u_ans = prompt.string('Your answer: ')
